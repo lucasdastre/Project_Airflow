7b404a54bc996c8b984e80f876f49171a68ddcb3
Lucas <lucasdastre10@gmail.com>
2025-09-05 18:17:32 -0300

    fix/env

--- include/db.py
+++ include/db.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = "postgresql://dbname_3e6o_user:yJvpCRJRyKmcSXt2wZk8JBaLavXP6cKV@dpg-d2tkopffte5s73abinqg-a.oregon-postgres.render.com/dbname_3e6o"
+SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRES_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
